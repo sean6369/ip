c502d0e9e10e1f4f21b6394f6a287fa246244421
Sean <seanleesukiat@gmail.com>
2025-08-29 01:18:27 +0800

    Add echo of commands
    Echos commands entered by the user
    Exits when the user types the command bye
    Add additional ASCII art for bot name

--- src/main/java/Finch.java
+++ src/main/java/Finch.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Finch {
 
     // Constant for the bot's name
@@ -9,11 +11,35 @@ public class Finch {
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("""
+                ███████╗██╗███╗   ██╗ ██████╗██╗  ██╗
+                ██╔════╝██║████╗  ██║██╔════╝██║  ██║
+                █████╗  ██║██╔██╗ ██║██║     ███████║
+                ██╔══╝  ██║██║╚██╗██║██║     ██╔══██║
+                ██║     ██║██║ ╚████║╚██████╗██║  ██║
+                ╚═╝     ╚═╝╚═╝  ╚═══╝ ╚═════╝╚═╝  ╚═╝
+                                                    \s""");
         line();
         System.out.println("Hello! I'm " + NAME);
         System.out.println("What can I do for you?");
         line();
+
+        String userInput;
+        while (true) {
+            System.out.println();
+            userInput = scanner.nextLine();
+            line();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            System.out.println(userInput);
+            line();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         line();
+        scanner.close();
     }
 }
